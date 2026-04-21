aeb4939aa5faa782657f6175a283d5dfb163567e
c0a250426f <c0a250426f@edu.teu.ac.jp>
2026-04-21 17:29:05 +0900

    カンマ後の空白追加 issue#1

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,7 +5,7 @@ import random
 import time
 
 WIDTH, HEIGHT = 1100, 650
-DELTA = {pg.K_UP:(0,-5), pg.K_DOWN:(0,5), pg.K_LEFT:(-5,0), pg.K_RIGHT:(5,0)}
+DELTA = {pg.K_UP:(0, -5), pg.K_DOWN:(0, 5), pg.K_LEFT:(-5, 0), pg.K_RIGHT:(5, 0)}
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
